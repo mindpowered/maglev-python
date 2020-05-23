5b11798befb7622e983a50a5649211ac25fccf18
Build <support@mindpowered.ca>
2020-05-23 20:05:10 +0000

    Version 0.4.12

---
+++ README.md
@@ -0,0 +1,25 @@
+
+MagLev
+======
+
+Contents
+========
+
+* [MagLev Python Library](#maglev-python-library)
+	* [Licensing Options](#licensing-options)
+	* [Support](#support)
+
+# MagLev Python Library
+
+
+MagLev
+## Licensing Options
+
+
+Additional [licensing options] are available. 
+## Support
+
+
+For bugfixes, please raise an issue in the [issue tracker]
+
+For feature requests, and general support, please see [support options]
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+    #long_description = fh.read()
+setuptools.setup(
+     name='mindpoweredinfra-maglev',
+     version='0.4.12',
+     description="maglev",
+     #long_description=long_description,
+     #long_description_content_type="text/markdown",
+     py_modules=['maglev'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
