9aea4386fbdf85944656c0bee4f0792ba675d87b
Build <support@mindpowered.ca>
2020-09-26 15:28:30 +0000

    Version 0.4.48

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
     #long_description = fh.read()
 setuptools.setup(
      name='mindpowered-maglev',
-     version='0.4.47',
+     version='0.4.48',
      description="MagLev",
      #long_description=long_description,
      #long_description_content_type="text/markdown",
